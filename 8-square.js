fb1072b27168121221ccb1a37e1d2422cbb8be59
Okindo <okindo.dev@gmail.com>
2025-07-14 02:41:36 +0300

    A script that prints a square

--- 8-square.js
+++ 8-square.js
@@ -2,6 +2,6 @@ const arg = process.argv.slice(2);
 const num = parseInt(arg[0]);
 if (Number.isInteger(num)){
 for (let i=0;i<num;i++){
-console.log('x'.repeat(num))}
+console.log('X'.repeat(num))}
 }
 else {console.log('Missing size')}
